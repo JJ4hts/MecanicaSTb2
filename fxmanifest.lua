f0735395de7d6be18462fda6f471c0ae2f507f67
Jhonne ramos <105169497+JJ4hts@users.noreply.github.com>
2024-10-30 11:12:57 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+-- shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+fx_version "adamant"
+game "gta5"
+
+this_is_a_map "yes"
+
+client_script {
+    "client/*.lua"
+}
+
+files {
+    "client/*.lua"
+}
+
+
+
+
+
+-- Entre na ryze para mais mapas e dumps! 
+
+
+
+    --  https://discord.gg/ryzedump
+    
+    -- https://ryzestore.com/
